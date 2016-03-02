1993ad12e7c186b9ff91ec277f7110de919ea35d
Roger Sherman <rsherman@viddler.com>
2016-03-02 15:32:47 -0500

    defined ranges

--- monthly_payslip.py
+++ monthly_payslip.py
@@ -1,12 +1,25 @@
 import csv
 
+
+with open('monthly_payslip.csv') as csvfile:
+    pay = csv.reader(csvfile)
+
+
 def monthly_payroll():
     ranges = [(0, 18200, 0, 0), (182001, 37000, .19, 0), (37001, 80000, .325, 3572), (80001, 180000, .37, 17547),
               (180001, 1000000, .45, 54547)]
 
+    def check_taxes():
+        for rng in ranges:
+            minimum = rng[0]
+            maximum = rng[1]
+            rate = rng[2]
+            baseTax = rng[3]
 
-with open('monthly_payslip.csv') as csvfile:
-    pay = csv.reader(csvfile)
+
+
+
+
     for item in pay:
         a = item[0]
         b = item[1]
