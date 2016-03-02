593fc483467b0f9568934e6233501b4ead30d60e
Roger Sherman <rsherman@viddler.com>
2016-03-02 11:46:01 -0500

    added an annoying amount of if/elifs. Need to get rid of some of that repeated code.

--- monthly_payslip.py
+++ monthly_payslip.py
@@ -11,7 +11,25 @@ with open('monthly_payslip.csv') as csvfile:
         d = int(item[3].rstrip('%')) * 0.001
         e = item[4]
         grossIncome = round(c / 12)
-        incomeTax = round((3572 + (c - 37000) * 0.325) / 12)
-        netIncome = grossIncome - incomeTax
-        _super = round(grossIncome * d)
-        print(name + "," + e + "," + str(grossIncome) + "," + str(netIncome) + "," + str(_super))
+        if c <= 18200:
+            incomeTax = 0
+        elif 18201 <= c <= 37000:
+            incomeTax = round(((c - 18200) * 0.19) / 12)
+            netIncome = grossIncome - incomeTax
+            _super = round(grossIncome * d)
+            print(name + "," + e + "," + str(grossIncome) + "," + str(netIncome) + "," + str(_super))
+        elif 37001 <= c <= 80000:
+            incomeTax = round((3572 + (c - 37000) * 0.325) / 12)
+            netIncome = grossIncome - incomeTax
+            _super = round(grossIncome * d)
+            print(name + "," + e + "," + str(grossIncome) + "," + str(netIncome) + "," + str(_super))
+        elif 80001 <= c <= 180000:
+            incomeTax = round((17547 + (c - 80000) * 0.37) / 12)
+            netIncome = grossIncome - incomeTax
+            _super = round(grossIncome * d)
+            print(name + "," + e + "," + str(grossIncome) + "," + str(netIncome) + "," + str(_super))
+        elif c >= 180001:
+            incomeTax = round((54547 + (c - 180000) * 0.45) / 12)
+            netIncome = grossIncome - incomeTax
+            _super = round(grossIncome * d)
+            print(name + "," + e + "," + str(grossIncome) + "," + str(netIncome) + "," + str(_super))
